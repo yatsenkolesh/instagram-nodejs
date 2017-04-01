42324af50042aa1648d8ed930ab2ca2b3256a695
webfay <webfay1@gmail.com>
2017-04-01 04:30:22 +0300

    Add methods for follow/unfollow

--- instagram.js
+++ instagram.js
@@ -193,4 +193,34 @@ module.exports = class Instagram
       console.log('Instagram authentication failed')
     )
   }
+
+  /**
+    * I did not want to implement this, but I need a stars on github
+    * If you use this library - star this rep https://github.com/yatsenkolesh/instagram-nodejs
+    * Thank you, bro
+    * Follow/unfollow user by id
+    * @param {int} userID
+    * @param {boolean} isUnfollow
+    * @return {object} Promise of fetch request
+  */
+  follow(userId, isUnfollow)
+  {
+    return fetch('https://www.instagram.com/web/friendships/'+userId+(isUnfollow ? '/unfollow' : '/follow'),
+    {
+      'method' : 'post',
+      'headers' :
+      {
+        'referer': 'https://www.instagram.com/',
+        'origin' : 'https://www.instagram.com',
+        'user-agent' : this.userAgent,
+        'x-instagram-ajax' : '1',
+        'x-requested-with' : 'XMLHttpRequest',
+        'x-csrftoken' : this.csrfToken,
+        cookie :' sessionid='+this.sessionId+'; csrftoken='+this.csrfToken
+      }
+    }).then(res =>
+    {
+      return res
+    })
+  }
 }
